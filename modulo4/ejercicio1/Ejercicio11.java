49428f2ec6b12bc7883929d0c3de670532de6bf9
SaleLol <106615384+SaleLol@users.noreply.github.com>
2023-06-29 15:33:52 -0300

    Create Ejercicio11.java

---
+++ modulo4/ejercicio1/Ejercicio11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VerificarVocal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un carácter: ");
+        char caracter = scanner.next().charAt(0);
+
+        boolean esVocal = (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u');
+
+        if (esVocal) {
+            System.out.println("El carácter ingresado es una vocal.");
+        } else {
+            System.out.println("El carácter ingresado no es una vocal.");
+        }
+    }
+}
