9ebf28ff2817eaf2a2bf7f88ed228f090ee0fc92
SaleLol <106615384+SaleLol@users.noreply.github.com>
2023-06-29 15:30:00 -0300

    Create Ejercicio8.java

---
+++ modulo4/ejercicio1/Ejercicio8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PiedraPapelTijera {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Jugador 1 - Ingrese su elección (0: piedra, 1: papel, 2: tijera): ");
+        int eleccion1 = scanner.nextInt();
+
+        System.out.print("Jugador 2 - Ingrese su elección (0: piedra, 1: papel, 2: tijera): ");
+        int eleccion2 = scanner.nextInt();
+
+        String ganador;
+
+        if (eleccion1 == eleccion2) {
+            ganador = "Empate";
+        } else if (eleccion1 == 0 && eleccion2 == 2 || eleccion1 == 1 && eleccion2 == 0 || eleccion1 == 2 && eleccion2 == 1) {
+            ganador = "Jugador 1";
+        } else {
+            ganador = "Jugador 2";
+        }
+
+        System.out.println("El ganador es: " + ganador);
+    }
+}
