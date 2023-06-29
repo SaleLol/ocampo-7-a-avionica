7fe5325c9e34628be8043fba818711b5d7d40a05
SaleLol <106615384+SaleLol@users.noreply.github.com>
2023-06-29 15:22:41 -0300

    Create Ejercicio2.java

---
+++ modulo4/ejercicio1/Ejercicio2.java
@@ -0,0 +1,11 @@
+public class ParImpar {
+    public static void main(String[] args) {
+        int numero = 17; // El número que deseas verificar
+
+        if (numero % 2 == 0) {
+            System.out.println("El número es par.");
+        } else {
+            System.out.println("El número es impar.");
+        }
+    }
+}
