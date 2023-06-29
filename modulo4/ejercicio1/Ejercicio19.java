190314419b2ca937fb8219e6c8804cf4a37824e5
SaleLol <106615384+SaleLol@users.noreply.github.com>
2023-06-29 16:00:36 -0300

    Create Ejercicio19.java

---
+++ modulo4/ejercicio1/Ejercicio19.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        int contador = 1;
+        int suma = 0;
+        double promedio = 0;
+        Random random = new Random();
+
+        while (contador <= 10) {
+            int numero = random.nextInt(100); // Genera un número aleatorio entre 0 y 99
+            System.out.println("Número " + contador + ": " + numero);
+            suma += numero;
+            contador++;
+        }
+
+        promedio = (double) suma / 10;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Promedio: " + promedio);
+    }
+}
+En este programa, se utiliza un ciclo while para generar y mostrar 10 números aleatorios. El contador se inicializa en 1 y se incrementa en cada iteración hasta llegar a 10.
+
+Dentro del ciclo, se genera un número aleatorio utilizando la clase Random. En cada iteración, se muestra el número generado y se acumula en la variable suma.
+
+Después de completar el ciclo while, se calcula el promedio dividiendo la suma entre 10 y se almacena en la variable promedio.
+
+
+
