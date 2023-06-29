e14498e46e35909eed99765fcb79e3ba23572fcb
SaleLol <106615384+SaleLol@users.noreply.github.com>
2023-06-29 15:28:17 -0300

    Create Ejercicio6.java

---
+++ modulo4/ejercicio1/Ejercicio6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Curso {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de curso: ");
+        int curso = scanner.nextInt();
+
+        String mensaje;
+
+        if (curso == 0) {
+            mensaje = "Jardín de infantes";
+        } else if (curso >= 1 && curso <= 6) {
+            mensaje = "Primaria";
+        } else if (curso >= 7 && curso <= 12) {
+            mensaje = "Secundaria";
+        } else {
+            mensaje = "Curso inválido";
+        }
+
+        System.out.println(mensaje);
+    }
+}
