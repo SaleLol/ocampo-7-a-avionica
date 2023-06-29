f38eaf17906821be288801917bd6da2524dcc378
SaleLol <106615384+SaleLol@users.noreply.github.com>
2023-06-29 15:24:38 -0300

    Create Ejercicio3.java

---
+++ modulo4/ejercicio1/Ejercicio3.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class DiasMes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el nombre del mes: ");
+        String nombreMes = scanner.nextLine();
+
+        int dias;
+
+        switch (nombreMes.toLowerCase()) {
+            case "enero":
+            case "marzo":
+            case "mayo":
+            case "julio":
+            case "agosto":
+            case "octubre":
+            case "diciembre":
+                dias = 31;
+                break;
+            case "febrero":
+                dias = 28;
+                break;
+            case "abril":
+            case "junio":
+            case "septiembre":
+            case "noviembre":
+                dias = 30;
+                break;
+            default:
+                dias = -1;
+                break;
+        }
+
+        if (dias != -1) {
+            System.out.println("El mes " + nombreMes + " tiene " + dias + " días.");
+        } else {
+            System.out.println("Nombre de mes inválido.");
+        }
+    }
+}
