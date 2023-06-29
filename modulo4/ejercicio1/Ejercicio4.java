259ad53319a869977015a559c6902b239fffb036
SaleLol <106615384+SaleLol@users.noreply.github.com>
2023-06-29 15:25:48 -0300

    Create Ejercicio4.java

---
+++ modulo4/ejercicio1/Ejercicio4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Categorias {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese una categoría (a, b o c): ");
+        String categoria = scanner.nextLine();
+
+        String palabra;
+
+        switch (categoria.toLowerCase()) {
+            case "a":
+                palabra = "hijo";
+                break;
+            case "b":
+                palabra = "padres";
+                break;
+            case "c":
+                palabra = "abuelos";
+                break;
+            default:
+                palabra = "Categoría inválida";
+                break;
+        }
+
+        System.out.println("Corresponde a la categoría: " + palabra);
+    }
+}
