c781e2cf97c5c8bc75aa4ec43e4dedf712b7f6fe
SaleLol <106615384+SaleLol@users.noreply.github.com>
2023-06-29 15:44:38 -0300

    Update Ejercicio 12.java

--- modulo4/ejercicio1/Ejercicio 12.java
+++ modulo4/ejercicio1/Ejercicio 12.java
@@ -7,12 +7,18 @@ public class DeterminarDocena {
         System.out.print("Ingrese un número: ");
         int numero = scanner.nextInt();
 
-        if (numero >= 1 && numero <= 12) {
-            System.out.println("El número pertenece a la primer docena.");
-        } else if (numero >= 13 && numero <= 24) {
-            System.out.println("El número pertenece a la segunda docena.");
-        } else if (numero >= 25 && numero <= 36) {
-            System.out.println("El número pertenece a la tercer docena.");
+        boolean primeraDocena = numero >= 1 && numero <= 12;
+        boolean segundaDocena = numero >= 13 && numero <= 24;
+        boolean terceraDocena = numero >= 25 && numero <= 36;
+
+        if (primeraDocena || segundaDocena || terceraDocena) {
+            if (primeraDocena) {
+                System.out.println("El número pertenece a la primer docena.");
+            } else if (segundaDocena) {
+                System.out.println("El número pertenece a la segunda docena.");
+            } else {
+                System.out.println("El número pertenece a la tercer docena.");
+            }
         } else {
             System.out.println("El número " + numero + " está fuera de rango.");
         }
